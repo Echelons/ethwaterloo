495500b4441a2ca8d7570a6e439c86cc72ecb6a3
Hunter Derkach <hunterderkach@gmail.com>
2017-10-14 23:21:55 -0400

    adds storage of trust and getter

--- truste/truste.sol
+++ truste/truste.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.11;
 contract Mortal {
     address owner;
 
-    function mortal() {
+    function Mortal() {
       owner = msg.sender;
     }
 
@@ -14,33 +14,51 @@ contract Mortal {
     }
 }
 
-contract BindingTrustE is Mortal {
-  address trustee;
-
-  function BindingTrustE(address _trustee) {
-    trustee = _trustee;
-  }
-
-  function andInTheDarknessBindThem() constant returns (bool) {
-    if (msg.sender == trustee) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-}
-
 contract TrustE is Mortal {
+
+  mapping(address => mapping(address => bool)) private councilOfElrond;
 
   event TrustAgreement(
     address addr,
     address ider
   );
 
+  event ForgeTrust(
+    address trstr,
+    address trste
+  );
+
   function initTrust(address _trustee) {
-    address addr = address(new BindingTrustE(_trustee));
+    address addr = address(new BindingTrustE(_trustee, owner));
     TrustAgreement(addr, msg.sender);
+  }
+
+  function forgeTrust(address _truster, address _trustee) {
+    councilOfElrond[_truster][_trustee] = true;
+    ForgeTrust(_truster, _trustee);
+  }
+
+  function isTrusted(address _truster, address _trustee) public constant returns (bool) {
+    return councilOfElrond[_truster][_trustee];
+  }
+
+}
+
+contract BindingTrustE is Mortal {
+  address trustee;
+  address truster;
+  TrustE fellowship;
+
+  function BindingTrustE(address _trustee, address _truster) {
+    trustee = _trustee;
+    truster = _truster;
+    fellowship = TrustE(msg.sender);
+  }
+
+  function andInTheDarknessBindThem() {
+    if (msg.sender == trustee) {
+      fellowship.forgeTrust(trustee, truster);
+    }
   }
 
 }
